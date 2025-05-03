3f86617567815f323cc259a525cdfc6b37180f86
dhlee56 <dhlee@ee.knu.ac.kr>
2025-05-03 15:50:15 +0900

    get back every thing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Header from "../components/Header";
-import Footer from "../components/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,10 +24,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="flex flex-col min-h-screen">
-        <Header />
-        <main className="flex-grow">{children}</main>
-        <Footer />
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
       </body>
     </html>
   );
--- src/app/posts/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Posts() {
-  return (
-    <div className="text-center mt-10">
-      <h1 className="text-3xl font-bold">Posts</h1>
-      <p className="mt-4">This is the Posts page. Here you can view all posts.</p>
-    </div>
-  );
-}
