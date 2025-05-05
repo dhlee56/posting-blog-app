075a159b73e79ffe930cdd347991bf46fced6784
dhlee56 <dhlee@ee.knu.ac.kr>
2025-05-05 14:03:00 +0900

    protect server action

--- src/app/create-post/actions.ts
+++ src/app/create-post/actions.ts
@@ -1,6 +1,14 @@
 "use server";
 
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { redirect } from "next/navigation";
+
 export async function handleCreatePost(formData: FormData) {
+  const { isAuthenticated } = getKindeServerSession();
+  if (!(await isAuthenticated())) {
+    redirect("/api/auth/login?redirectUrl=/create-post");
+    //post_login_redirect_url
+  }
   const title = formData.get("title");
   const body = formData.get("body");
 
