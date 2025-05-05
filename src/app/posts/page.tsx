3d6118c9a544f025b070c89b8bf6c869491c7c16
dhlee56 <dhlee@ee.knu.ac.kr>
2025-05-05 10:01:56 +0900

    Updated posts page so that header aligned with titles and content shown top of the screen

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -5,9 +5,9 @@ export default async function Posts() {
   const data = await res.json();
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen p-8 text-center bg-amber-400">
-      <h1 className="text-4xl font-bold mb-4">Posts</h1>
-      <ul className="w-full max-w-2xl text-left">
+    <div className="flex flex-col items-center justify-start min-h-screen p-8 text-center">
+      <h1 className="w-full max-w-2xl text-left text-4xl font-bold mb-4 ">Posts</h1>
+      <ul className="w-full max-w-2xl text-left ">
         {data.posts.map((post: { id: number; title: string }) => (
           <li key={post.id} className="mb-2">
             <Link
