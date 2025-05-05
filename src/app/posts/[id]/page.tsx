cb5f2ad6cdaee5c0dc878666fca95c9013b46ec1
dhlee56 <dhlee@ee.knu.ac.kr>
2025-05-05 10:05:04 +0900

    Updated posts/[id] page

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -9,7 +9,7 @@ export default async function Post({ params }: PostProps) {
   const post = await res.json();
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen p-8 text-center">
+    <div className="flex flex-col items-center justify-start min-h-screen p-8 text-center">
       <h1 className="text-4xl font-bold mb-4">{post.title}</h1>
       <p className="text-lg text-gray-600">{post.body}</p>
     </div>
